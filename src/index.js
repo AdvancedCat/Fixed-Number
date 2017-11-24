25b26537cdce561cdf8545a14a4bd95641b47ea5
xinhong <xinhong@anjuke.com>
2017-11-24 13:12:03 +0800

    mod:update code

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ var previousFN = root.FN;
 
 var FN = {};
 
+
 if (typeof exports != 'undefined' && !exports.nodeType) {
 	if (typeof module != 'undefined' && !module.nodeType && module.exports) {
 	    exports = module.exports = FN;
